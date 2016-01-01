e1cc05e6be449c5f41a67ff63375946240f9f67e
Anton Österberg <anton.osterberg@gmail.com>
2016-01-01 15:48:50 +0100

    Silly me tried to import a synth. I do not need that...

--- src/dimensions/client/engine/GameThreadExecutor.java
+++ src/dimensions/client/engine/GameThreadExecutor.java
@@ -5,8 +5,6 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import com.sun.swing.internal.plaf.synth.resources.synth;
-
 class GameThreadExecutor extends ScheduledThreadPoolExecutor
 {
 	private boolean isPaused = false;
